1abfe2efa3b6685d3d7535f516a48fc5de6bfaec
juanmesa2097 <juanmesa2097@gmail.com>
2021-09-29 18:36:02 -0500

    Refactor code

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
-import express from "express";
-import { getDownloadsFolder } from "platform-folders";
-import { runFilesDetection } from "./scripts/files";
+import { config } from 'dotenv';
+import express from 'express';
+import { getDownloadsFolder } from 'platform-folders';
+import { runFilesDetection } from './scripts/files';
 
+// config env variables
+config();
+
+// create express app
 const app = express();
+app.get('/', (req, res) => res.send('Indexing local server'));
 
-app.get("/", (req, res) => res.send("Indexing local server"));
-
+// run file changing detection
 const downloadsPath = getDownloadsFolder();
 runFilesDetection(downloadsPath);
 
+// init server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log(`Server listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
--- scripts/files.ts
+++ scripts/files.ts
@@ -1,28 +1,33 @@
-import { watch } from "chokidar";
-import { readFile, readFileSync } from "fs";
-import { Logger } from "./logger";
+import { watch } from 'chokidar';
+import { readFileSync } from 'fs';
+import { Logger } from './logger';
 
-export const runFilesDetection = (path: string) => {
-  const watcher = watch(path, {
-    ignored: /(^|[\/\\])\../, // ignore dotfiles,
-    persistent: true,
-  });
-
-  watcher
-    .on("add", (path) => {
-      Logger.log(`File ${path} has been added`);
-
-      const fileContent = readFileSync(path, { encoding: "base64" });
-      console.log(fileContent);
-    })
-    .on("change", (path) => {
-      Logger.log(`File ${path} has been changed`);
-
-      readFile(path, (buffer) => {
-        console.log(buffer);
-      });
-    })
-    .on("unlink", (path) => {
-      Logger.log(`File ${path} has been removed`);
+export const runFilesDetection = (
+  path: string,
+): Promise<{ eventName: 'add' | 'change' | 'unlink'; result: string }> => {
+  return new Promise((resolve, reject) => {
+    const watcher = watch(path, {
+      ignored: /(^|[\/\\])\../, // ignore dotfiles,
+      persistent: true,
     });
+
+    watcher
+      .on('add', (path) => {
+        Logger.log(`File ${path} has been added`);
+
+        const fileContent = readFileSync(path, { encoding: 'base64' });
+        resolve({ eventName: 'add', result: fileContent });
+      })
+      .on('change', (path) => {
+        Logger.log(`File ${path} has been changed`);
+
+        const fileContent = readFileSync(path, { encoding: 'base64' });
+        resolve({ eventName: 'change', result: fileContent });
+      })
+      .on('unlink', (path) => {
+        Logger.log(`File ${path} has been removed`);
+
+        resolve({ eventName: 'unlink', result: path });
+      });
+  });
 };
