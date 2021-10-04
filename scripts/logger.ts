116eb5ce143204d32ee464b7698d121486dc4e70
juanmesa2097 <juanmesa2097@gmail.com>
2021-10-04 14:27:16 -0500

    Remove typescript

--- scripts/logger.ts
+++
@@ -1,6 +0,0 @@
-const log = console.log.bind(console);
-const error = console.error.bind(console);
-const info = console.info.bind(console);
-const warn = console.warn.bind(console);
-
-export const Logger = { log, error, info, warn };
